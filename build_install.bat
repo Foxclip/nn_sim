194eaef9785e9b05c1c61e60fc9be5eb800a9ce9
Foxclip <destroyApp2@gmail.com>
2020-07-31 22:48:09 +1000

    Clearing dist folder on build

--- build_install.bat
+++ build_install.bat
@@ -1,3 +1,12 @@
+:: clearing dist folder
+cd /d dist
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+cd ..
+
+:: building .whl package
 python setup.py sdist bdist_wheel
+
+:: installing package
 pip install --upgrade dist/nn_sim-0.0.1-py3-none-any.whl
+
 pause
